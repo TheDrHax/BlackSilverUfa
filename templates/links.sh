8e0fbbf5e5a04308bbb5d1d3d57f7bce93935e70
Дмитрий Карих <the.dr.hax@gmail.com>
2017-12-17 01:14:37 +0300

    Просмотр видео прямо из каталога
    * Плеер открывается при нажатии на ссылку ▶;
    * Старый плеер можно открыть в новой вкладке через ту же ссылку (по
    крайней мере пока);
    * Скрипты полностью вынесены в CDN;

--- templates/links.sh
+++ templates/links.sh
@@ -28,22 +28,40 @@ parse_stream() {
     CHAT_LINK="[v$TWITCH.ass](../chats/v$TWITCH.ass)"
     if echo $YOUTUBE | grep -q NULL; then
         YOUTUBE_LINK="Запись отсутствует"
-        PLAYER_CMD="streamlink -p \"mpv --sub-file chats/v$TWITCH.ass\" --player-passthrough hls twitch.tv/videos/$TWITCH best"
         PLAYER_LINK=""
+        PLAYER_CMD="streamlink -p \"mpv --sub-file chats/v$TWITCH.ass\" --player-passthrough hls twitch.tv/videos/$TWITCH best"
     else
         YOUTUBE_LINK="[$YOUTUBE](https://www.youtube.com/watch?v=$YOUTUBE)"
+        PLAYER_LINK="<a href=\"/src/player.html?v=$YOUTUBE&s=$TWITCH\" onclick=\"return openPlayer$TWITCH()\">▶</a>"
         PLAYER_CMD="mpv --sub-file chats/v$TWITCH.ass ytdl://$YOUTUBE"
-        PLAYER_LINK="[▶](../src/player.html?v=$YOUTUBE&s=$TWITCH)"
     fi
 
-cat <<EOF
+    cat <<EOF
 ## $NAME
 
 | Twitch | Субтитры | YouTube | ▶ |
 | ------ | -------- | ------- | - |
 | $TWITCH_LINK | $CHAT_LINK | $YOUTUBE_LINK | $PLAYER_LINK |
 
-### Команда для запуска плеера
+<script>
+  function openPlayer$TWITCH() {
+    createPlayer("player-$YOUTUBE", "$YOUTUBE", "$TWITCH");
+    document.getElementById("spoiler-$YOUTUBE").click();
+    return false;
+  }
+</script>
+
+<details>
+  <summary id="spoiler-$YOUTUBE"></summary>
+
+  <div class="player-wrapper" style="margin-top: 32px">
+    <video
+      id="player-$YOUTUBE"
+      class="video-js vjs-default-skin vjs-big-play-centered" />
+  </div>
+</details>
+
+#### Команда для просмотра стрима в проигрывателе MPV
 
 \`\`\`
 $PLAYER_CMD
@@ -56,7 +74,48 @@ generate_page() {
     TITLE=$(head -n1 "$list")
     LINES=$(grep -nE '(--)' "$list" | sed 's/:.*//g')
 
-cat <<EOF
+    cat <<EOF
+<!-- video.js -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/video.js/6.3.3/video-js.css" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/video.js/6.3.3/video.js"></script>
+<!-- videojs-youtube -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/videojs-youtube/2.4.1/Youtube.js"></script>
+<!-- libjass -->
+<link href="https://cdn.jsdelivr.net/npm/libjass@0.11.0/libjass.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/libjass@0.11.0/libjass.js"></script>
+<!-- videojs-ass -->
+<link href="https://cdn.jsdelivr.net/npm/videojs-ass@0.8.0/src/videojs.ass.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/videojs-ass@0.8.0/src/videojs.ass.js"></script>
+<!-- videojs-resolution-switcher -->
+<script src="https://cdn.jsdelivr.net/npm/videojs-resolution-switcher@0.4.2/lib/videojs-resolution-switcher.min.js"></script>
+
+<script>
+function createPlayer(id, youtube, twitch) {
+  videojs(id, {
+    controls: true,
+    nativeControlsForTouch: false,
+    width: 640,
+    height: 360,
+    fluid: true,
+    plugins: {
+      ass: {
+        src: ["../chats/v" + twitch + ".ass"],
+        delay: -0.1,
+      },
+      videoJsResolutionSwitcher: {
+        default: 'high',
+        dynamicLabel: true
+      }
+    },
+    techOrder: ["youtube"],
+    sources: [{
+      "type": "video/youtube",
+      "src": "https://www.youtube.com/watch?v=" + youtube
+    }]
+  });
+}
+</script>
+
 # $TITLE
 
 `
@@ -72,7 +131,7 @@ done
 * \`git clone https://github.com/TheDrHax/BlackSilverUfa.git\`
 * \`cd BlackSilverUfa\`
 * \`git checkout gh-pages\`
-* Команда из таблицы выше
+* Команда, приведённая выше
 
 EOF
 }
