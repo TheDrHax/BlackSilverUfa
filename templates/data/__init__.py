63fd1a405e347161c1a834232447124abc614d79
Дмитрий Карих <the.dr.hax@gmail.com>
2018-05-11 16:43:13 +0300

    Служебные объекты вынесены в отдельный класс

--- templates/data/__init__.py
+++ templates/data/__init__.py
@@ -1,18 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import json
-
-
-def load_json(filename):
-    with open(filename, "r") as f:
-        return json.load(f)
-
-
-class AttrDict(dict):
-    def __init__(self, *args, **kwargs):
-        super(AttrDict, self).__init__(*args, **kwargs)
-        self.__dict__ = self
+from .utils import load_json, AttrDict
 
 
 class Stream(AttrDict):
