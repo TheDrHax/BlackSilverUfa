fb9741bd7b31179f69942af5e9aae04feff36328
Jenkins <the.dr.hax@gmail.com>
2018-06-07 13:04:12 +0000

    Jenkins: Обновление статичных файлов

--- search.js
+++ search.js
@@ -23,7 +23,6 @@ var Redirect = {
     "250258247": "/links/co-op.html#250258247",
     "259446831": "/links/co-op.html#259446831",
     "268497396": "/links/co-op.html#268497396",
-    "268497396.1": "/links/co-op.html#268497396.1",
     "234037742.1": "/links/warhammer-vermintide-2.html#234037742.1",
     "236864236.1": "/links/warhammer-vermintide-2.html#236864236.1",
     "236864236.2": "/links/warhammer-vermintide-2.html#236864236.2",
@@ -340,8 +339,7 @@ var Search = {
     "Dead Dozen": "/links/co-op.html#236864236",
     "Depth / Dead Realm / Worms W.M.D / SoulHunt / Deceit": "/links/co-op.html#250258247",
     "Conan Exiles": "/links/co-op.html#259446831",
-    "Worms W.M.D / Raft": "/links/co-op.html#268497396",
-    "Murderous Pursuits / The Forest": "/links/co-op.html#268497396.1",
+    "Worms W.M.D / Raft / Murderous Pursuits / The Forest": "/links/co-op.html#268497396",
   },
 
   select: function (value) {
