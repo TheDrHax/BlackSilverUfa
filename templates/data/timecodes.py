b1e20298bba02a84302de8fbfc5d06b11e2aed8a
Дмитрий Карих <the.dr.hax@gmail.com>
2018-07-01 22:39:57 +0300

    Хранить названия таймкодов в объектах Timecode
    На один костыль стало меньше.

--- templates/data/timecodes.py
+++ templates/data/timecodes.py
@@ -29,7 +29,7 @@ class Timecode(object):
             s = s % i
         return ':'.join(result)
 
-    def __init__(self, timecode):
+    def __init__(self, timecode, name=None):
         if (type(timecode) is str):
             value = self.text_to_sec(timecode)
         elif (type(timecode) is int):
@@ -39,6 +39,7 @@ class Timecode(object):
         else:
             value = 0
 
+        self.name = name
         self.value = abs(value)
         self.negative = value < 0
 
@@ -46,7 +47,7 @@ class Timecode(object):
         return ('-' if self.negative else '') + self.sec_to_text(self.value)
 
     def __repr__(self):
-        return 'Timecode({})'.format(str(self))
+        return 'Timecode({}, {})'.format(str(self), self.name)
 
     def __int__(self):
         return (-1 if self.negative else 1) * self.value
@@ -58,11 +59,11 @@ class Timecode(object):
 
     def __add__(self, other):
         self._type_check(other)
-        return Timecode(int(self) + int(other))
+        return Timecode(int(self) + int(other), self.name)
 
     def __sub__(self, other):
         self._type_check(other)
-        return Timecode(int(self) - int(other))
+        return Timecode(int(self) - int(other), self.name)
 
     def __ge__(self, other):
         self._type_check(other)
@@ -84,54 +85,50 @@ class Timecode(object):
 class Timecodes(list):
     offset = Timecode(0)
 
-    def __init__(self, timecodes={}):
+    def __init__(self, timecodes={}, name=None):
         if type(timecodes) is not dict:
             raise TypeError(type(timecodes))
 
+        self.name = name
         for key, value in timecodes.items():
             try:
-                t = Timecode(key)
-                self.append((t, value))
+                self.append(Timecode(key, value))
             except ValueError:
-                t = Timecodes(value)
-                self.append((t, key))
+                self.append(Timecodes(value, key))
 
     def __int__(self):
-        if len(self) > 0:
-            return int(self[0][0])
-        else:
-            return 0
+        return int(self[0]) if len(self) > 0 else 0
 
     def values(self):
         return sorted(self)
 
     def append(self, value):
-        if type(value) is not tuple:
+        if type(value) not in [Timecode, Timecodes]:
             raise TypeError(type(value))
         for i, item in enumerate(self):
-            if item[0] >= value[0]:
+            if item >= value:
                 return self.insert(i, value)
         super(Timecodes, self).append(value)
-        self.value = int(self[0][0])
+        self.value = int(self[0])
 
     def start_at(self, offset):
         self.offset += offset
 
         for value in self.copy():
-            if type(value[0]) is Timecode and offset > value[0]:
+            if type(value) is Timecode and offset > value:
                 self.remove(value)
-            elif type(value[0]) is Timecodes:
-                value[0].start_at(offset)
-                if len(value[0]) == 0:
+            elif type(value) is Timecodes:
+                value.start_at(offset)
+                if len(value) == 0:
                     self.remove(value)
 
-        for i, (time, name) in enumerate(self.copy()):
-            if type(time) is Timecode:
-                self[i] = (time - offset, name)
+        for i, t in enumerate(self.copy()):
+            if type(t) is Timecode:
+                self[i] = t - offset
 
     def end_at(self, offset):
         for value in self.copy():
-            if offset - self.offset <= value[0]:
+            if offset - self.offset <= value:
                 self.remove(value)
 
 
