2493fefd80d8c29f6bdab9fd466789efbbfae2c8
Дмитрий Карих <the.dr.hax@gmail.com>
2018-09-14 22:07:59 +0300

    Единый пакет utils

--- generate.py
+++ generate.py
@@ -5,8 +5,7 @@ import os
 import shutil
 from mako.lookup import TemplateLookup
 from templates.data import streams, games, categories
-from templates.data.utils import load_json
-from templates.utils import _
+from templates.utils import _, load_json
 
 
 lookup = TemplateLookup(directories=['./templates'],
