bd7bb20d1f2f8ce56ada32a96c6a463fc4ec418d
Дмитрий Карих <the.dr.hax@gmail.com>
2018-10-01 02:52:43 +0300

    Генерация статистики оптимизирована за счет кэша
    Сборка стала происходить более чем в 2 раза быстрее (3.425 → 1.534 
    секунды).

--- templates/utils/__init__.py
+++ templates/utils/__init__.py
@@ -31,6 +31,36 @@ def md5file(f_path, block_size=2**20):
     return md5.hexdigest()
 
 
+# https://stackoverflow.com/a/18603065
+def last_line(fp):
+    if not os.path.isfile(fp):
+        return None
+
+    with open(fp, 'rb') as f:
+        f.seek(-2, os.SEEK_END)
+        while f.read(1) != b"\n":
+            f.seek(-2, os.SEEK_CUR)
+        return f.readline().decode('utf-8')
+
+
+# https://stackoverflow.com/a/1019572
+def count_lines(fp):
+    if not os.path.isfile(fp):
+        return None
+
+    with open(fp, 'r') as f:
+        return sum(1 for line in f)
+
+
+def dir_size(dir):
+    if not os.path.isdir(dir):
+        return 0
+
+    return sum(os.path.getsize(dir + '/' + f)
+               for f in os.listdir(dir)
+               if os.path.isfile(dir + '/' + f))
+
+
 def numword(num, word):
     p = morph.parse(word)[0]
     return '{} {}'.format(num, p.make_agree_with_number(num).word)
