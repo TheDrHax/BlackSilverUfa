9f3d042cfa1d604aee99cc6eb17924ad57bb3221
Jenkins <the.dr.hax@gmail.com>
2019-01-05 20:02:08 +0000

    Jenkins: Обновление статичных файлов

--- search.js
+++ search.js
@@ -277,6 +277,7 @@ var Redirect = {
     "354466172": "/links/subnautica.html#354466172",
     "358216034": "/links/subnautica.html#358216034",
     "358681563": "/links/subnautica.html#358681563",
+    "359212700": "/links/subnautica.html#359212700",
     "352766083": "/links/sw-kotor2.html#352766083",
     "349397154.1": "/links/lucius-3.html#349397154.1",
     "348930664.1": "/links/ashen.html#348930664.1",
