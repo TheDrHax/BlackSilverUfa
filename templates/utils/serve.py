516698f2f0d79b2691408b39756e3dfed9f86af1
Дмитрий Карих <the.dr.hax@gmail.com>
2019-01-18 03:11:18 +0300

    Docker заменён на виртуальное окружение Python 3
    Так как в проекте произошло множество упрощений (особенно отказ от шаблонов на Bash-скриптах), теперь необязательно тащить с собой предварительно настроенный контейнер.

--- templates/utils/serve.py
+++ templates/utils/serve.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
 from subprocess import call
 from livereload import Server
 
 from . import _
 
 
+python = os.environ.get('PYTHON') or 'python3'
+
+
 def generate():
-    return call(['python', '-m', 'templates.utils.generate'])
+    return call([python, '-m', 'templates.utils.generate'])
 
 
 def serve(host='0.0.0.0', port=8000, root=_('')):
