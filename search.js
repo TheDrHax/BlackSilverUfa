8169792c42873e6844ec637fe42c0e2eb2af3de8
Jenkins <the.dr.hax@gmail.com>
2019-06-21 22:31:35 +0300

    Jenkins: Обновление статичных файлов

--- search.js
+++ search.js
@@ -563,6 +563,7 @@ var Redirect = {
     "439860739": "/links/hollow-knight.html#439860739",
     "440294828": "/links/hollow-knight.html#440294828",
     "440765344": "/links/hollow-knight.html#440765344",
+    "442171113": "/links/first.html#442171113",
   },
 
   link: function (hash) {
@@ -814,6 +815,11 @@ var Search = {
       {name: "Quake 2 RTX", path: "/links/first.html#438447673", year: 2019},
       {name: "Stay in the Light", path: "/links/first.html#438447673", year: 2019},
       {name: "Minecraft RTX", path: "/links/first.html#438447673", year: 2019},
+      {name: "Mario Royale", path: "/links/first.html#442171113", year: 2019},
+      {name: "TMNT Resque-Palooza", path: "/links/first.html#442171113", year: 2019},
+      {name: "Crash Team Racing", path: "/links/first.html#442171113", year: 2019},
+      {name: "My Friend Pedro", path: "/links/first.html#442171113", year: 2019},
+      {name: "Bloodstained: Ritual of the Night", path: "/links/first.html#442171113", year: 2019},
     ],
     "ongoing": [
       {name: "Persona 5", path: "/links/persona_5.html", year: 2017},
