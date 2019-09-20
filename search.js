d5976522fff9c4f3b0cd78ba33626f8ee1ad0d7d
Jenkins <the.dr.hax@gmail.com>
2019-09-20 18:50:48 +0300

    Jenkins: Обновление статичных файлов

--- search.js
+++ search.js
@@ -626,6 +626,7 @@ var Redirect = {
     "482664774": "/links/first.html#482664774",
     "483098038": "/links/mk-11.html#483098038",
     "483585370": "/links/first.html#483585370",
+    "483918141": "/links/first.html#483918141",
   },
 
   link: function (hash) {
@@ -765,6 +766,7 @@ var Search = {
     ],
     "first": [
       { name: "Первый взгляд", path: "/links/first.html", year: 2019 },
+      { name: "The Legend of Zelda: Link's Awakening", path: "/links/first.html#483918141", year: 2019 },
       { name: "Police Stories", path: "/links/first.html#483585370", year: 2019 },
       { name: "Neon Abyss (демо)", path: "/links/first.html#482664774", year: 2019 },
       { name: "SCP-087-B", path: "/links/first.html#481343199", year: 2019 },
