320f03691460fcc6b9847ce8784520131571f987
Дмитрий Карих <the.dr.hax@gmail.com>
2019-10-21 22:16:08 +0300

    Калибровка TimecodeHelper

--- templates/data/timecodes.py
+++ templates/data/timecodes.py
@@ -274,7 +274,7 @@ class TimecodesSlice(Timecodes):
 
 
 class TimecodeHelper:
-    offset = 11  # Constant for experimental low latency streamlink + MPV without cache
+    offset = 10  # Constant for experimental low latency streamlink + MPV without cache
 
     @staticmethod
     def time():
