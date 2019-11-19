5a888c6b6228f83cb6eaf1b39c4953e4434d0d6a
Jenkins <the.dr.hax@gmail.com>
2019-11-19 22:23:28 +0300

    Jenkins: Обновление статичных файлов

--- search.js
+++ search.js
@@ -673,6 +673,7 @@ var Redirect = {
     "508178551": "/links/events.html#508178551",
     "508580428": "/links/star-wars-jedi.html#508580428",
     "509067029": "/links/star-wars-jedi.html#509067029",
+    "510472726": "/links/first.html#510472726",
   },
 
   link: function (hash) {
@@ -821,6 +822,8 @@ var Search = {
     ],
     "first": [
       { name: "Первый взгляд", path: "/links/first.html", year: 2019 },
+      { name: "Terminator: Resistance", path: "/links/first.html#510472726", year: 2019 },
+      { name: "Shenmue III", path: "/links/first.html#510472726", year: 2019 },
       { name: "Untitled Goose Game", path: "/links/first.html#508178551", year: 2019 },
       { name: "Planet Zoo", path: "/links/first.html#504672487", year: 2019 },
       { name: "Need for Speed Heat", path: "/links/first.html#504248004", year: 2019 },
