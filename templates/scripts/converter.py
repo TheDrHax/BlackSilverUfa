7cf2d1b5e90af8edf4efcff6ddf5f3804c842380
Дмитрий Карих <the.dr.hax@gmail.com>
2020-01-02 00:50:41 +0300

    tcd обновлён до версии 2.4.1
    Теперь при загрузке субтитров группируются не только эмоуты, но и целые словосочетания до 5 слов.

--- templates/scripts/converter.py
+++ templates/scripts/converter.py
@@ -2,53 +2,103 @@
 # -*- coding: utf-8 -*-
 
 import os
-from datetime import timedelta, datetime as dtt
+import re
+from tcd.twitch import Message
+from multiprocessing import Pool
+# from datetime import timedelta, datetime as dtt
 
 
-T_MIN = 2
-T_MAX = 5
-MSG_MAX = 100
+# T_MIN = 2
+# T_MAX = 5
+# MSG_MAX = 100
 
 
-def ptime(t):
-    return dtt.strptime(t, '%H:%M:%S.%f')
+# def ptime(t):
+#     return dtt.strptime(t, '%H:%M:%S.%f')
 
 
-def ftime(t):
-    return dtt.strftime(t, '%-H:%M:%S.%f')[:-4]
+# def ftime(t):
+#     return dtt.strftime(t, '%-H:%M:%S.%f')[:-4]
 
 
-def convert_time(t, msg_len):
-    part = (MSG_MAX - min(msg_len, MSG_MAX)) / MSG_MAX
-    duration = T_MAX - (T_MAX - T_MIN) * part
-    return t + timedelta(seconds=duration)
+# def convert_time(t, msg_len):
+#     part = (MSG_MAX - min(msg_len, MSG_MAX)) / MSG_MAX
+#     duration = T_MAX - (T_MAX - T_MIN) * part
+#     return t + timedelta(seconds=duration)
 
 
-def convert_line(input):
-    msg_split = input.split(', ', 9)
+PACKED_EMOTE = re.compile('([^\ ]+) x⁣([0-9]+)')
 
-    msg = msg_split[9]
-    msg_len = len(msg[msg.index(': ') + 2:])
 
-    t_start = ptime(msg_split[1])
-    t_end = convert_time(t_start, msg_len)
+class EmptyLineError(Exception):
+    pass
 
-    msg_split[2] = ftime(t_end)
+
+def unpack_emotes(line):
+    result = line
+
+    for m in reversed(list(PACKED_EMOTE.finditer(line))):
+        mg = m.groups()
+        ms = m.span()
+
+        emote = mg[0]
+        count = int(mg[1])
+
+        if count > 200:
+            print(f'Ignoring line: {line}')
+            continue
+
+        result = ''.join((result[:ms[0]],
+                          ' '.join([emote] * int(count)),
+                          result[ms[1]:]))
+        
+        if len(result) > 500:
+            print(f'{len(result)}/500 chars: {line}')
+            return line
+
+    return result
+
+
+def convert_line(line):
+    msg_split = line.split(', ', 9)
+
+    try:
+        username, text = msg_split[9].split(': ', 1)
+    except Exception as ex:
+        raise EmptyLineError()
+
+    # Unpack / Repack emotes
+    text = unpack_emotes(text)
+    text = Message.group(text, group_format='{emote} x⁣{count}')
+    msg_split[9] = f'{username}: {text}'
+
+    # Convert message durations
+    # msg_len = len(msg[msg.index(': ') + 2:])
+    # t_start = ptime(msg_split[1])
+    # t_end = convert_time(t_start, msg_len)
+    # msg_split[2] = ftime(t_end)
+
     return ', '.join(msg_split)
 
 
-def convert_file(input):
-    with open(input, 'r') as f_in, open(input + '.tmp', 'w') as f_out:
+def convert_file(input_file):
+    print('Converting ' + input_file)
+
+    with open(input_file, 'r') as f_in, open(input_file + '.tmp', 'w') as f_out:
         for line in f_in:
-            if line.startswith('Dialogue: '):
-                line = convert_line(line)
+            try:
+                if line.startswith('Dialogue: '):
+                    line = convert_line(line.strip()) + '\n'
 
-            f_out.write(line)
+                f_out.write(line)
+            except EmptyLineError:
+                continue
 
-    os.rename(input + '.tmp', input)
+    os.rename(input_file + '.tmp', input_file)
 
 
 if __name__ == '__main__':
-    for i in os.listdir('../_site/chats/'):
-        print('Converting ' + i)
-        convert_file('../_site/chats/' + i)
+    p = Pool(8)
+    l = map(lambda x: './_site/chats/' + x, os.listdir('./_site/chats/'))
+    p.map(convert_file, l)
+    p.close()
