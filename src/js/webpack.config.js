023d074e248b19c61975900ef0fef7930b395b61
Дмитрий Карих <the.dr.hax@gmail.com>
2020-03-04 19:00:08 +0300

    Webpack: Упакованы Plyr и SubtitlesOctopus

--- src/js/webpack.config.js
+++ src/js/webpack.config.js
@@ -1,4 +1,5 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = [
   {
@@ -35,6 +36,46 @@ module.exports = [
       new MiniCssExtractPlugin({
         filename: 'bundle.css'
       }),
+    ]
+  },
+  {
+    entry: [
+      './src/js/player.js',
+      './src/css/player.scss',
+    ],
+    stats: 'errors-only',
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'cache-loader',
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            'cache-loader',
+            'babel-loader',
+          ]
+        }
+      ],
+    },
+    output: {
+      filename: 'player.js',
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'player.css'
+      }),
+      new CopyPlugin([{
+        from: 'node_modules/libass-wasm/dist/*worker*',
+        flatten: true
+      }]),
     ]
   }
 ];
