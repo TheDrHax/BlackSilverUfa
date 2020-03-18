591d16f416908ceb2d706744f1e530370dedf02c
Дмитрий Карих <the.dr.hax@gmail.com>
2020-03-18 16:30:45 +0300

    search.js: Поиск по ID стрима (issue #21)

--- src/js/search.js
+++ src/js/search.js
@@ -1,10 +1,11 @@
 import autocomplete from 'autocompleter';
+import { Redirect } from './redirect';
 
 class Search {
   static games = null;
 
   static strip(string) {
-    return string.trim().toLowerCase().match(/[a-zа-я0-9]+/gi).join(' ');
+    return string.trim().toLowerCase().match(/[a-zа-я0-9\.]+/gi).join(' ');
   }
 
   static async load() {
@@ -29,11 +30,27 @@ class Search {
   static async init(selector) {
     let games = await Search.load();
 
+    await Redirect.init();
+    let segments = Object.keys(Redirect.segments);
+
     return autocomplete({
       minLength: 2,
       input: document.querySelector(selector),
       fetch: (text, update) => {
         text = Search.strip(text);
+
+        if (segments.indexOf(text) !== -1) {
+          update(segments.filter((key) => key.startsWith(text)).map((key) => {
+            let segment = Redirect.segments[key];
+            return {
+              name: segment.name,
+              group: 'Переход по ID',
+              url: segment.url
+            };
+          }));
+          return;
+        }
+
         let suggestions = games.filter((x) => {
           if (text.indexOf(' ') != -1) {
             return Search.strip(x.name).indexOf(text) != -1;
@@ -42,11 +59,14 @@ class Search {
             return words.filter(y => y.startsWith(text)).length > 0;
           }
         });
+
         update(suggestions);
       },
       render: (item, currentValue) => {
         let div = document.createElement("div");
-        div.innerHTML = item.name + ' - <span>' + item.year + '</span>';
+        let html = item.name;
+        if (item.year) { html += ' - <span>' + item.year + '</span>'; }
+        div.innerHTML = html;
         return div;
       },
       onSelect: (item) => {
