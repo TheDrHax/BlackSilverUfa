78ddb9cac332d0350ff61e9cbf83a878f9927210
Дмитрий Карих <the.dr.hax@gmail.com>
2020-07-24 12:33:39 +0300

    Обновлён экстрактор чата из IRC
    Это резервный вариант, который позволяет получить субтитры, даже если они уже удалены с Twitch.
    
    Всё ещё остаётся проблема с модерацией, т.к. в субтитрах, полученных таким образом, будут видны все удалённые сообщения.

--- src/scripts/irc.py
+++ src/scripts/irc.py
@@ -1,25 +1,27 @@
-HELP = """
+"""Usage: irc <source> <start> <duration> <vod>
+
 This script converts IRC logs to chat subtitles.
 
-Usage: ./bsu python -m src.scripts.irc <INPUT> <START> <DURATION> <ID>
-    INPUT - Name of IRC log file
-    START - Date and time of stream start (ISO 8601, UTC)
-    DURATION - Length of stream (HH:MM:SS)
-    ID - Twitch VOD ID
+source      Path or URL of IRC log file
+start       Date and time of stream start (ISO 8601, UTC)
+duration    Length of stream (HH:MM:SS)
+vod         Twitch VOD ID
 """
 
-import tcd
-import sys
 
 from datetime import datetime, timedelta
+
+import requests
+from docopt import docopt
 from tcd.twitch import Message
+from tcd.settings import settings as tcd_settings
 from tcd.subtitles import SubtitleWriter
 
-from ..utils import load_json
+from ..data.config import tcd_config
 from ..data.timecodes import Timecode
 
 
-tcd.settings.update(load_json('data/tcd.json'))
+tcd_settings.update(tcd_config)
 
 
 def irc_params(params):
@@ -30,50 +32,63 @@ def irc_params(params):
         parts = p.split('=')
         result[parts[0]] = parts[1]
 
-    return result        
+    return result
 
 
-def parser(input: str, start: datetime, duration: datetime):
-    with open(input, 'r') as fi:
-        for line in fi:
-            parts = line.split(' ')
-            ts = datetime.fromisoformat(parts[0])
+def parser(source: str, start: datetime, duration: datetime):
+    if source.startswith('http'):
+        fi = requests.get(source).content.decode('utf-8').split('\n')
+    else:
+        fi = open(source, 'r')
 
-            if ts < start or parts[1] != 'privmsg':
-                continue
+    for line in fi:
+        parts = line.split(' ')
 
-            if ts > start + duration:
-                break
-            
-            params = irc_params(parts[2])
-            user = parts[3][1:]
-            text = ' '.join(parts[4:])[1:].strip()
+        if parts[1] != 'privmsg':
+            continue
 
-            yield Message({
-                'commenter': {
-                    'display_name': params['display-name']
-                },
-                'message': {
-                    'body': text,
-                    'user_color': (params.get('color') or '#FFFFFF')[1:]
-                },
-                'content_offset_seconds': (ts - start).total_seconds()
-            })
+        # For some reason IRC log is behind chat by 11 seconds
+        # This needs to be confirmed for more streams
+        ts = datetime.fromisoformat(parts[0]) - timedelta(seconds=11)
+
+        if ts < start:
+            continue
+        elif ts > start + duration:
+            break
+
+        params = irc_params(parts[2])
+        message = ' '.join(parts[4:])[1:].strip()
+
+        yield Message({
+            'commenter': {
+                'display_name': params['display-name']
+            },
+            'message': {
+                'body': message,
+                'user_color': (params.get('color') or '#FFFFFF')[1:]
+            },
+            'content_offset_seconds': (ts - start).total_seconds()
+        })
+
+    if not isinstance(fi, list):
+        fi.close()
+
+
+def main(argv=None):
+    args = docopt(__doc__, argv=argv)
+
+    source = args['<source>']
+    start = datetime.fromisoformat(args['<start>'])
+    duration = timedelta(seconds=int(Timecode(args['<duration>'])))
+    vod = args['<vod>']
+
+    writer = SubtitleWriter(vod)
+
+    for msg in parser(source, start, duration):
+        writer.add(msg)
+
+    writer.close()
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 5:
-        print(HELP)
-        sys.exit(1)
-    
-    INPUT = sys.argv[1]
-    START = datetime.fromisoformat(sys.argv[2])
-    DURATION = timedelta(seconds=int(Timecode(sys.argv[3])))
-    ID = sys.argv[4]
-
-    writer = tcd.subtitles.SubtitleWriter(ID)
-    
-    for msg in parser(INPUT, START, DURATION):
-        writer.add(msg)
-    
-    writer.close()
+    main()
