42c5715221d59c216a31f12c9f9507342b9656b0
Дмитрий Карих <the.dr.hax@gmail.com>
2020-08-01 16:07:39 +0300

    Обновлена задержка в TimecodeHelper

--- src/utils/repl.py
+++ src/utils/repl.py
@@ -7,7 +7,6 @@ from ..data.timecodes import Timecode, TimecodeHelper
 STATE = 'https://red.thedrhax.pw/blackufa/twitch'
 
 c = Channel('blackufa')
-v = lambda: c.videos().__next__()
 t = TimecodeHelper('0:00:00')
 
 
