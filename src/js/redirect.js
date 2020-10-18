11ce822d74a27ca151cf70f0681b0d8f901df59a
Дмитрий Карих <the.dr.hax@gmail.com>
2020-10-19 01:21:42 +0300

    Ссылки на таймкоды и стримы на конкретных страницах (fixes #32)
    Данное изменение добавляет возможность создания коротких ссылок, ведущих на таймкоды. Также теперь ссылки могут указывать на какую именно игру должно произойти перенаправление (раньше выбиралась первая подходящая).
    
    Ссылки имеют следующий вид: https://blackufa.thedrhax.pw/r/?игра/сегмент/таймкод

--- src/js/redirect.js
+++ src/js/redirect.js
@@ -26,31 +26,94 @@ class Redirect {
     return hash.match(SEGMENT_HASH_REGEX) !== null;
   }
 
-  static async link(hash) {
-    if (!hash || hash.length == 0) {
-      return '/';
+  static async parse(hash) {
+    if (!hash) {
+      return null;
     }
 
-    if (!Redirect.isHash(hash)) { // Assume ID of a game
-      return '/links/' + hash + '.html';
+    let parts = hash.split('/');
+    let game = null, segment = null, timecode = null;
+
+    if (!Redirect.isHash(parts[0])) {
+      [game, segment, timecode] = parts;
+
+      if (!segment) {
+        return {
+          game: game,
+          segment: segment,
+          timecode: timecode
+        };
+      }
+    } else {
+      [segment, timecode] = parts;
+    }
+
+    // Strip .0 from segments
+    if (segment.endsWith('.0')) {
+      segment = segment.substr(0, segment.length - 2);
     }
 
     let segments = await Redirect.init();
 
-    if (hash.indexOf(',') !== -1 && !Object.keys(segments).includes(hash)) {
-      hash = hash.split(',')[0];
+    if (Object.keys(segments).indexOf(segment) === -1) {
+      if (segment.indexOf(',') !== -1) { // Redirect from removed joined streams
+        let joined_parts = segment.split(',');
+        let found = false;
+
+        for (let i = 0; i < joined_parts.length; i++) {
+          let part = joined_parts[i];
+
+          if (Object.keys(segments).indexOf(part) !== -1) {
+            segment = part;
+            found = true;
+            break;
+          }
+
+          if (!found) {
+            return null;
+          }
+        }
+      } else {
+        return null;
+      }
     }
 
-    if (segments[hash]) {
-      return segments[hash].url;
-    } else {
-      return '/';
+    let segment_data = segments[segment];
+
+    if (segment && (!game || segment_data.games.indexOf(game) === -1)) {
+      game = segment_data.games[0];
     }
+
+    return {
+      game: game,
+      segment: segment,
+      timecode: timecode
+    };
   }
 
-  static async go(hash) {
+  static async link(raw_hash) {
+    let hash = await Redirect.parse(raw_hash);
+
+    if (!hash) {
+      return '/';
+    }
+
+    let url = `/links/${hash.game}.html`;
+
+    if (hash.segment) {
+      url += `#${hash.segment}`;
+    }
+
+    if (hash.timecode) {
+      url += `?t=${hash.timecode}`;
+    }
+
+    return url;
+  }
+
+  static async go(raw_hash) {
     let path = document.location.pathname + document.location.hash;
-    let dest = await Redirect.link(hash);
+    let dest = await Redirect.link(raw_hash);
 
     if (path != dest) {
       window.location.replace(dest);
@@ -58,13 +121,7 @@ class Redirect {
   }
 
   static detect() {
-    let hash = window.location.search.substring(1);
-
-    if (Redirect.isHash(hash) && hash.endsWith('.0')) {
-      hash = hash.split('.')[0];
-    }
-
-    Redirect.go(hash);
+    Redirect.go(window.location.search.substr(1));
   }
 }
 
