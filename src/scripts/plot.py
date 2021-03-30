5713622100e58c0d57ae40878fa21603c7be85ba
Дмитрий Карих <the.dr.hax@gmail.com>
2021-03-31 01:14:09 +0300

    plot: Скрипт для построения графиков по словам из чата
    Это минимальный скрипт, который принимает на вход ID стрима и несколько слов и выдаёт график частоты употребления этих слов на стриме.
    
    Проверив этот скрипт на последних нескольких стримах, я заметил корелляцию между таймкодами, клипами и чатом, так что можно использовать этот скрипт для поиска интересных моментов для таймкодов.

---
+++ src/scripts/plot.py
@@ -0,0 +1,46 @@
+"""Usage: plot <stream> <keyword>..."""
+
+import re
+
+import plotly.graph_objects as go
+from docopt import docopt
+
+from ..data.streams import streams
+from ..data.timecodes import Timecode
+from ..utils.ass import SubtitlesReader
+from .converter import unpack_line_breaks, unpack_emotes
+
+
+STEP=30
+
+
+def main(argv=None):
+    args = docopt(__doc__, argv=argv)
+
+    r = SubtitlesReader(streams[args['<stream>']].subtitles_path)
+    matcher = re.compile(f'({"|".join(args["<keyword>"])})')
+    data = []
+
+    for event in r.events():
+        text = unpack_line_breaks(event.text)
+        text = unpack_emotes(text)
+
+        start = int(event.start // STEP)
+
+        for i in range(start - len(data) + 1):
+            data.append(0)
+
+        data[start] += len(matcher.findall(text))
+
+    r.close()
+
+    x = [str(Timecode(x * STEP)) for x in range(len(data))]
+    y = data
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=x, y=data))
+    fig.show()
+
+
+if __name__ == '__main__':
+    main()
