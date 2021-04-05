46ebb446ca69999994673bab82ef4245db77657a
Дмитрий Карих <the.dr.hax@gmail.com>
2021-04-06 00:32:06 +0300

    Поделиться: Показывать текущий таймкод

--- src/js/components/player/share-overlay.js
+++ src/js/components/player/share-overlay.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import { sum, reverse, range, padStart } from 'lodash';
 import { Button, Col, Form, InputGroup, Popover } from 'react-bootstrap';
+
+function ptime(t) {
+  return sum(reverse(t.split(':')).map((x, i) => x * 60 ** i));
+}
+
+function ftime(t) {
+  return range(2, -1, -1).map((i) => {
+    const res = Math.floor(t / 60 ** i);
+    t %= 60 ** i;
+    return padStart(res, 2, '0');
+  }).join(':');
+}
 
 const ShareOverlay = React.forwardRef((props, ref) => {
   const {
@@ -11,7 +24,7 @@ const ShareOverlay = React.forwardRef((props, ref) => {
   } = props;
 
   const inputRef = React.useRef();
-  const [time, setTime] = React.useState(Math.floor(currentTime));
+  const time = Math.floor(currentTime);
   const [includeTime, setIncludeTime] = React.useState(true);
 
   let baseUrl = `https://bsu.us.to/${game}/${segment}`;
@@ -21,7 +34,7 @@ const ShareOverlay = React.forwardRef((props, ref) => {
   }
 
   return (
-    <Popover ref={ref} {...otherProps}>
+    <Popover ref={ref} {...otherProps} className="share-popover">
       <Popover.Title as="h3">Создать короткую ссылку</Popover.Title>
       <Popover.Content>
         <Form.Row>
@@ -49,14 +62,24 @@ const ShareOverlay = React.forwardRef((props, ref) => {
           </Col>
         </Form.Row>
 
-        <Form.Row>
-          <Form.Check
-            className="m-2"
-            type="checkbox"
-            label="Начать с текущего момента"
-            checked={includeTime}
-            onChange={({ target: { checked } }) => setIncludeTime(checked)}
-          />
+        <Form.Row className="mt-2">
+          <Col className="d-flex align-content-center">
+            <Form.Check
+              className="mr-2"
+              type="checkbox"
+              label="Начать с таймкода: "
+              checked={includeTime}
+              onChange={({ target: { checked } }) => setIncludeTime(checked)}
+            />
+
+            <Form.Control
+              className="time-selector"
+              value={ftime(time)}
+              size="sm"
+              readOnly
+              htmlSize={4}
+            />
+          </Col>
         </Form.Row>
       </Popover.Content>
     </Popover>
