c5db28f1c6b5f31e827b18095093be707e5f4175
zaprvalcer <zaprvalcer@gmail.com>
2021-10-17 18:41:02 +0300

    Фикс обработки ентера при вводе поисковой строки

--- src/js/pages/search-page/text-filter.js
+++ src/js/pages/search-page/text-filter.js
@@ -5,8 +5,17 @@ import { Button, Form, InputGroup } from 'react-bootstrap';
 // Namespace
 import { searchPage as t } from '../../constants/texts';
 
+const ENTER_KEYS = ['NumpadEnter', 'Enter'];
+
 const TextFilter = ({ onSubmit }) => {
   const [value, setValue] = useState('');
+
+  const handleKeyPress = (event) => {
+    if (!ENTER_KEYS.includes(event.code)) return;
+
+    event.preventDefault();
+    onSubmit(value);
+  };
 
   return (
     <>
@@ -15,7 +24,7 @@ const TextFilter = ({ onSubmit }) => {
         value={value}
         placeholder={t.queryPlaceholder}
         onChange={({ target }) => setValue(target.value)}
-        onKeyPress={(event) => event.code === 'Enter' && onSubmit(value)}
+        onKeyPress={handleKeyPress}
       />
       <InputGroup.Append>
         <Button variant="primary" onClick={() => onSubmit(value)}>{t.search}</Button>
