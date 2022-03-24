3a0ba75a9d63dfc42da62b5e8efb9ff83a9b37c2
Дмитрий Карих <the.dr.hax@gmail.com>
2022-03-24 03:46:41 +0300

    Сообщения об ошибках через Sentry

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,11 @@ import '../css/styles.scss';
 
 import React from 'react';
 import { render } from 'react-dom';
+import * as Sentry from '@sentry/react';
 import App from './app';
+
+Sentry.init({
+  dsn: 'https://5763ba8387e642b690528a52adead2ef@o1176364.ingest.sentry.io/6274115',
+});
 
 render(<App />, document.getElementById('app'));
