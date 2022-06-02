9d706fcc9068f10df15817c5ab8fd7fa20979bbb
Дмитрий Карих <the.dr.hax@gmail.com>
2022-06-02 14:21:27 +0300

    ErrorPage: Запретить индексирование поисковиками

--- src/js/pages/error-page/index.js
+++ src/js/pages/error-page/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
+import { Helmet } from 'react-helmet-async';
 import { common as t } from '../../constants/texts';
 import PATHS from '../../constants/urls';
 import { Layout } from '../../components';
 
 const ErrorPage = () => (
   <Layout flex title={t.notFoundTitle}>
+    <Helmet>
+      <meta name="robots" content="noindex" />
+    </Helmet>
     <div className="flex-grow-1 d-flex flex-column justify-content-center align-items-center">
       <h3 className="text-white">
         {t.notFoundTitle}
